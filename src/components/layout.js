2310e544cbc7ec8cc61f820af47d8140fce4e424
Travis <travis@denouncetheclock.com>
2021-09-25 15:46:00 +1000

    Small fixes builds still fail

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link, useStaticQuery, graphql } from 'gatsby';
+import { Link } from 'gatsby';
 import { container, internalContainer, heading, navLinks, navLinkItem, navLinkText, siteTitle } from './layout.module.css';
 import { DesignLine } from './design-line/design-line';
 
